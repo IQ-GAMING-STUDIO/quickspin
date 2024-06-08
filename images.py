77171e00323c7581744662c638d511f758eba4ab
IQ2 <hotdogracingiq@gmail.com>
2024-06-08 12:48:40 +0300

    IQ

--- images.py
+++ images.py
@@ -1,5 +1,5 @@
-apple = 'APPLE_ICON'
-banana = 'BANANA_ICON'
-pear = 'PEAR_ICON'
+apple = 'https://drive.google.com/uc?export=view&id=1FHbhHtDegECxBStYCCYXv1Mv1KShSFhv'
+banana = 'https://drive.google.com/uc?export=view&id=1khKr49vOf9vLvPOh7XhoZBzCOhvg_7B1'
+pear = 'https://drive.google.com/uc?export=view&id=1ZGk-O4H90p38mQ-YJFEpL8vv5npPNjhw'
 
-question_mark = "QUESTION_MARK_ICON"
+question_mark = "https://drive.google.com/uc?export=view&id=1gnVL3_f2UggUJC7R085Bak5J2Jb-TDq9"
